8bbc41ca127171a1cb300d415719cc3b595b4b3f
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-04-06 08:52:08 +0300

    feat: solve Task2

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -1,7 +1,23 @@
 #import "Combinator.h"
 
 @implementation Combinator
+- (int) binomRecursiveBy:(int) n andBy: (int) k {
+  
+    if ((k==0) || (n==k))
+    {return 1;}
+    else
+    {return ([self binomRecursiveBy: (n-1) andBy:(k)]+[self binomRecursiveBy:(n-1) andBy:(k-1)]);}
+}
+
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    int c = [[array objectAtIndex:0] intValue];
+    int realN = [[array objectAtIndex:1]intValue];
+    int i = 1;
+    while (i<=realN) {
+         if ([self binomRecursiveBy: realN andBy:i] == c)
+            return [NSNumber numberWithInt:(i)];
+        i++;
+    }
+    return nil;
 }
 @end
